a2eb375c8aefe96889f3871835d6f81cb78770c6
vtmagno <vcmagno@up.edu.ph>
2019-04-21 20:43:56 +0800

    LookLearn with Showing Contacts on Chat loading messages

--- app/Http/Controllers/ContactsController.php
+++ app/Http/Controllers/ContactsController.php
@@ -11,10 +11,24 @@ class ContactsController extends Controller
 {
     public function get()
     {
+
+        // get all users except the logged in one
        $contacts = User::where('id', '!=', auth()->id())->get();
-       
-       return response()->json($contacts);
-    }
+
+        // get a collection of items where sender_id is the user who sent us a message
+        // and messages_count is the number of unread messages we have from him
+        $unreadIds = Message::select(\DB::raw('`from` as sender_id, count(`from`) as messages_count'))
+            ->where('to', auth()->id())
+            ->where('read', false)
+            ->groupBy('from')
+            ->get();
+            
+        // add an unread key to each contact with the count of unread messages
+        $contacts = $contacts->map(function($contact) use ($unreadIds) {
+            $contactUnread = $unreadIds->where('sender_id', $contact->id)->first();
+            $contact->unread = $contactUnread ? $contactUnread->messages_count : 0;
+            return $contact;
+        });
 
     public function getMessagesFor($id)
     {
