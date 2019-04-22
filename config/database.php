fbc971f6261a402183deeaa696166c6258bf6735
vtmagno <vcmagno@up.edu.ph>
2019-04-22 22:28:17 +0800

    edit db config

--- config/database.php
+++ config/database.php
@@ -42,7 +42,7 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'host' => env('DB_HOST', '127.0.0.1'),
+            'host' => env('DB_HOST', 'db4free.net'),
             'port' => env('DB_PORT', '3306'),
             'database' => env('DB_DATABASE', 'looklearn'),
             'username' => env('DB_USERNAME', 'vcmagno'),
@@ -61,11 +61,11 @@ return [
 
         'pgsql' => [
             'driver' => 'pgsql',
-            'host' => env('DB_HOST', '127.0.0.1'),
+            'host' => env('DB_HOST', 'db4free.net'),
             'port' => env('DB_PORT', '5432'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'database' => env('DB_DATABASE', 'looklearn'),
+            'username' => env('DB_USERNAME', 'vcmagno'),
+            'password' => env('DB_PASSWORD', '!!)$(%theathea'),
             'charset' => 'utf8',
             'prefix' => '',
             'prefix_indexes' => true,
