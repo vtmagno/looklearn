e8a702c3271a65e9b5825f0a020cbe43c2187a46
vtmagno <vcmagno@up.edu.ph>
2019-04-22 22:29:17 +0800

    edit db config

--- config/database.php
+++ config/database.php
@@ -75,11 +75,11 @@ return [
 
         'sqlsrv' => [
             'driver' => 'sqlsrv',
-            'host' => env('DB_HOST', 'localhost'),
+            'host' => env('DB_HOST', 'db4free.net'),
             'port' => env('DB_PORT', '1433'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'database' => env('DB_DATABASE', 'looklearn'),
+            'username' => env('DB_USERNAME', 'vcmagno'),
+            'password' => env('DB_PASSWORD', '!!)$(%theathea'),
             'charset' => 'utf8',
             'prefix' => '',
             'prefix_indexes' => true,
