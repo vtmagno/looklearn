81da6789347e66406ab09f02c8ef6a31852cd527
vtmagno <vcmagno@up.edu.ph>
2019-04-23 19:08:37 +0800

    config

--- config/database.php
+++ config/database.php
@@ -42,21 +42,17 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'host' => env('DB_HOST', 'db4free.net'),
-            'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'looklearn'),
-            'username' => env('DB_USERNAME', 'vcmagno'),
-            'password' => env('DB_PASSWORD', '!!)$(%theathea'),
-            'unix_socket' => env('DB_SOCKET', ''),
+            'host' => 'db4free.net',
+            'port' => '33060',
+            'database' => 'looklearn',
+            'username' => 'vcmagno',
+            'password' => '!!)$(%theathea',
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
             'prefix' => '',
             'prefix_indexes' => true,
             'strict' => true,
             'engine' => null,
-            'options' => extension_loaded('pdo_mysql') ? array_filter([
-                PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
-            ]) : [],
         ],
 
         'pgsql' => [
